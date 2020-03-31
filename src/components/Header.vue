67acf95cb2fc0e84fe4e534ad035ddbf0f6fdbae
Caleb Hester <naclcaleb@gmail.com>
2020-03-30 18:17:03 -0700

    More links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,18 +8,28 @@
             @click='logOut'
             v-if='isAuthenticated'
         >
-            <a-menu-item key='1'>
-                Log Out
+            <a-sub-menu>
+                <span slot='title'><a-icon type='github'/>GitHub Repos</span>
+                <a-menu-item key='1'>
+                    Log Out
+                </a-menu-item>
+                <a-menu-item key='2'>
+                    <a href='https://github.com/highlowapp/highlowbackend' target='_blank'>Backend</a>
+                </a-menu-item>
+                <a-menu-item key='3'>
+                    <a href='https://github.com/highlowapp/ios-app' target='_blank'>iOS</a>
+                </a-menu-item>
+                <a-menu-item key='4'>
+                    <a href='https://github.com/highlowapp/android-app' target='_blank'>Android</a>
+                </a-menu-item>
+            </a-sub-menu>
+            <a-menu-item key='5'>
+                <a href='https://play.google.com/apps/publish/?account=7751825972145461522#AppListPlace' target='_blank'>Google Play</a>
             </a-menu-item>
-            <a-menu-item key='2'>
-                <a href='https://github.com/highlowapp/highlowbackend' target='_blank'>Backend</a>
+            <a-menu-item key='6'>
+                <a href='https://console.cloud.google.com/home/dashboard?project=highlow' target='_blank'>Google Cloud</a>
             </a-menu-item>
-            <a-menu-item key='3'>
-                <a href='https://github.com/highlowapp/ios-app' target='_blank'>iOS</a>
-            </a-menu-item>
-            <a-menu-item key='4'>
-                <a href='https://github.com/highlowapp/android-app' target='_blank'>Android</a>
-            </a-menu-item>
+
         </a-menu>
     </a-layout-header>
 </template>
